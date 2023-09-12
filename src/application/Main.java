73e52d152b77e3d8c6fa537f92a9e6d6be82c3fe
Sean <seand@momoven.es>
2023-09-12 09:33:11 -0700

    Added implementation of the selection tool

--- src/application/Main.java
+++ src/application/Main.java
@@ -41,8 +41,12 @@ public class Main extends Application {
 	private int SNAP = 10;
 	
 	private ArrayList<Shape> shapes;
-	private ArrayList<Line> lines;
+	private ArrayList<DraftLine> lines;
 	private ArrayList<Point> points;
+
+	private MousePressEventHandler mpeh = new MousePressEventHandler();
+	private MouseDragEventHandler mdeh = new MouseDragEventHandler();
+	private MouseReleaseEventHandler mreh = new MouseReleaseEventHandler();
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -91,9 +95,9 @@ public class Main extends Application {
 			background = new Rectangle(0,0,WIDTH,HEIGHT);
 			background.setFill(backgroundColor);
 			
-			drawingBoard.setOnMousePressed(new MousePressEventHandler());
-			drawingBoard.setOnMouseDragged(new MouseDragEventHandler());
-			drawingBoard.setOnMouseReleased(new MouseReleaseEventHandler());
+			drawingBoard.setOnMousePressed(mpeh);
+			drawingBoard.setOnMouseDragged(mdeh);
+			drawingBoard.setOnMouseReleased(mreh);
 			
 			//create axis gridlines
 			vertical = new Line(WIDTH/2,0,WIDTH/2,HEIGHT);
@@ -105,7 +109,7 @@ public class Main extends Application {
 			
 			//initialize the shapes and lines arrays
 			shapes = new ArrayList<Shape>();
-			lines = new ArrayList<Line>();
+			lines = new ArrayList<DraftLine>();
 			points = new ArrayList<Point>();
 			
 			root = new BorderPane();
@@ -123,21 +127,126 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-
+	/**
+	 * The points class describe points in the drawing
+	 * keeping track of the points is useful for snapping to them when drawing
+	 * 
+	 * a point has x and y coordinates
+	 * getDistance() returns the distance between two points
+	 * 
+	 * @author Sean Donaghy
+	 *
+	 */
+	public class Point extends Shape{
+		
+		private static int radius = 2;
+		private Circle pointCircle;
+		private double x,y;
+		
+		public Point(double x,double y) {
+			
+			this.x = x;
+			this.y = y;
+			
+		}
+		
+		public double getY() {
+			return this.y;
+		}
+		public void setY(double y) {
+			this.y = y;
+		}
+		public double getX() {
+			return this.x;
+		}
+		public void setX(double x) {
+			this.x = x;
+		}
+		public void setCircle(double x,double y) {
+			
+			this.pointCircle = new Circle(x,y,radius);
+			this.pointCircle.setFill(Color.WHITE);
+		}
+		public Circle getCircle() {
+			return this.pointCircle;
+		}
+		public double getDistance(Point other){
+			
+			return Math.sqrt(Math.pow(this.x-other.getX(),2)+
+						     Math.pow(this.y-other.getY(),2));
+		}
+	}
 	public class DraftLine extends Line {
 		
 		private Point start,end, mid;
 		private double angle;
 		private boolean isSelected;
 		private Color color;
+		private static final int LINE_WEIGHT = 2;
 
 		public DraftLine(double startX, double startY, double endX, double endY){
 			super(startX,startY,endX,endY);
 
-			start = new Point(startX,startY);
-			end = new Point(endX,endY);
-			isSelected = false;
-			
+			this.start = new Point(startX,startY);
+			this.end = new Point(endX,endY);
+			this.isSelected = false;
+			this.setStroke(Color.WHITE);
+
+			this.setStrokeWidth(LINE_WEIGHT);
+		}
+
+		public Point getStart() {
+			return start;
+		}
+
+		public void setStart(Point start) {
+			this.start = start;
+		}
+
+		public Point getEnd() {
+			return end;
+		}
+
+		public void setEnd(Point end) {
+			this.end = end;
+		}
+
+		public Point getMid() {
+			return mid;
+		}
+
+		public void setMid(Point mid) {
+			this.mid = mid;
+		}
+
+		public double getAngle() {
+			return angle;
+		}
+
+		public void setAngle(double angle) {
+			this.angle = angle;
+		}
+
+		public boolean isSelected() {
+			return isSelected;
+		}
+
+		public void setSelected(boolean isSelected) {
+			this.isSelected = isSelected;
+			if (isSelected) {
+				this.setStroke(Color.LIGHTBLUE);
+			}
+			else{
+				this.setStroke(Color.WHITE);
+			}
+		}
+
+		public Color getColor() {
+			return color;
+		}
+
+		public void setColor(Color color) {
+			this.color = color;
 		}
 	}
 	/**
@@ -149,14 +258,6 @@ public class Main extends Application {
 		
 		@Override
 		public void handle(MouseEvent e) {
-			
-			//wherever the mouse is pressed, all lines should return to not selected
-			//this really highlights the fact that a draftLine class is needed
-			for(Line l:lines) {
-			
-			l.setFill(Color.WHITE);
-			
-			}
 			
 			boolean nearPoint = false;
 			Point mousePos = new Point(e.getX(),e.getY());
@@ -204,26 +305,30 @@ public class Main extends Application {
 				
 				//this will create a line where both the start and end points are intitially where the mouse
 				//click occurred or if the mouse was close enough to a point, the line will start at that point
-				Line drawLine = new Line(start.getX(),start.getY(),start.getX(),start.getY());
-				drawLine.setStrokeWidth(2);
-				drawLine.setStroke(Color.WHITE);
+				DraftLine draftLine = new DraftLine(start.getX(),start.getY(),start.getX(),start.getY());
+				draftLine.setStart(start);
+				
 				//all lines are added to an Array list so that they can be changed
-				lines.add(drawLine);
+				lines.add(draftLine);
+				shapes.add(draftLine);
 				
 				//Add a circle for the start point
-				start.setCircle(start.getX(),start.getY());
+				draftLine.getStart().setCircle(start.getX(),start.getY());
+				//start.setCircle(start.getX(),start.getY());
 				
 				//the user should be able to start or finish a line on top of a point(point circle)
-				start.getCircle().setOnMousePressed(new MousePressEventHandler());
-				start.getCircle().setOnMouseDragged(new MouseDragEventHandler());
-				start.getCircle().setOnMouseReleased(new MouseReleaseEventHandler());
+				draftLine.getStart().getCircle().setOnMousePressed(mpeh);
+				draftLine.getStart().getCircle().setOnMouseDragged(mdeh);
+				draftLine.getStart().getCircle().setOnMouseReleased(mreh);
 				
 				//add the start point to the Array List points to keep track of all points
-				points.add(start);
-				drawingBoard.getChildren().add(start.getCircle());
+				points.add(draftLine.getStart());
+				shapes.add(draftLine.getStart());
+
+				drawingBoard.getChildren().add(draftLine.getStart().getCircle());
 			
 				//add the line to the drawing
-				drawingBoard.getChildren().add(drawLine);
+				drawingBoard.getChildren().add(draftLine);
 				
 				//add the line dimension to the drawing
 				dimension = new Text("");
@@ -231,24 +336,52 @@ public class Main extends Application {
 				drawingBoard.getChildren().add(dimension);
 				
 				//the user should also be able to start a new line when they have clicked on a previously drawn line
-				drawLine.setOnMousePressed(new MousePressEventHandler());
-				drawLine.setOnMouseDragged(new MouseDragEventHandler());
-				//drawLine.setOnMouseReleased(new MouseReleaseEventHandler());
+				draftLine.setOnMousePressed(mpeh);
+				draftLine.setOnMouseDragged(mdeh);
+				
+				for(Shape s:shapes){
+					s.setOnMousePressed(mpeh);
+					s.setOnMouseDragged(mdeh);
+					s.setOnMouseReleased(mreh);
+					
+				}
 			}
 			
 			//Entity Selection Tool
-			//This doesn't work yet
 			if(select.isSelected()) {
 				
-				if(e.getSource() instanceof Line) {
+				if(e.getSource() instanceof DraftLine) {
 					
-					selectedLine = (Line)e.getSource();
-					selectedLine.setFill(Color.LIGHTBLUE);
+					DraftLine selectedLine = (DraftLine) e.getSource();
+					selectedLine.setSelected(true);
+
+					e.consume();
 					
+				}
+
+				else{
+					deselectAll();
 				}
 				
 			}
-			
+
+			else{
+				deselectAll();
+			}
+
+		}
+		/**
+		 * This method will deselect all drawing entities
+		 */
+		private void deselectAll(){
+
+			//once more entities are implemented they should all be stored in a single array list of type Shape
+			//deselect all Draft Lines
+			for(DraftLine l:lines) {
+	
+				l.setSelected(false);
+		
+			}
 		}
 	}
 	/**
@@ -390,9 +523,9 @@ public class Main extends Application {
 				endPoint = new Point(currentLine.getEndX(),currentLine.getEndY());
 				endPoint.setCircle(endPoint.getX(),endPoint.getY());
 				
-				endPoint.getCircle().setOnMousePressed(new MousePressEventHandler());
-				endPoint.getCircle().setOnMouseDragged(new MouseDragEventHandler());
-				endPoint.getCircle().setOnMouseReleased(new MouseReleaseEventHandler());
+				endPoint.getCircle().setOnMousePressed(mpeh);
+				endPoint.getCircle().setOnMouseDragged(mdeh);
+				endPoint.getCircle().setOnMouseReleased(mreh);
 				
 				points.add(endPoint);
 				
@@ -401,58 +534,7 @@ public class Main extends Application {
 			}
 		}
 	}
-	/**
-	 * The points class describe points in the drawing
-	 * keeping track of the points is useful for snapping to them when drawing
-	 * 
-	 * a point has x and y coordinates
-	 * getDistance() returns the distance between two points
-	 * 
-	 * @author Sean Donaghy
-	 *
-	 */
-	public class Point extends Shape{
-		
-		private static int radius = 2;
-		private Circle pointCircle;
-		private double x,y;
-		
-		public Point(double x,double y) {
-			
-			this.x = x;
-			this.y = y;
-			
-		
-		}
-		
-		public double getY() {
-			return this.y;
-		}
-		public void setY(double y) {
-			this.y = y;
-		}
-		public double getX() {
-			return this.x;
-		}
-		public void setX(double x) {
-			this.x = x;
-		}
-		public void setCircle(double x,double y) {
-			
-			this.pointCircle = new Circle(x,y,radius);
-			this.pointCircle.setFill(Color.WHITE);
-		}
-		public Circle getCircle() {
-			return this.pointCircle;
-		}
-		public double getDistance(Point other){
-			
-			return Math.sqrt(Math.pow(this.x-other.getX(),2)+
-						     Math.pow(this.y-other.getY(),2));
-		}
-		
-		
-	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
